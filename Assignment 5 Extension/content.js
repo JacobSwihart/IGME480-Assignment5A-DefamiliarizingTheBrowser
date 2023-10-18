7c766d097266d604f0c9401a4b1fa403e288a1c3
JacobSwihart <70299421+JacobSwihart@users.noreply.github.com>
2023-10-17 22:12:25 -0400

    Cleaned up the files
    Added comments to the content.js file and cleaned up some unused code, added a title and description to the manifest.json file

--- Assignment 5 Extension/content.js
+++ Assignment 5 Extension/content.js
@@ -1,22 +1,15 @@
+//gets all the links on the page
 let el = document.querySelectorAll("a");
 for(let i = 0; i < el.length; i++){
+    //replaces the text content, title and any alt text with question marks to prevent the user from identifying the link without clicking on it
     el[i].innerHTML = "????";
     el[i].title = "????";
     el[i].alt = "????";
+    //stores the url before changing it to a function that does nothing
     el[i].dataset.url = el[i].href;
     el[i].href = "javascript:void(0);"
+    //when the link is clicked, the original url is restored, allowing the links to remain functional
     el[i].onclick = (function(){
         this.href = this.dataset.url;
     });
 }
-
-let style = document.querySelectorAll("style");
-for(let i = 0; i < style.length; i++){
-    if(style[i].innerHTML.indexOf("popups") != -1){
-        style[i].remove();
-    }
-    
-} 
-
-let a = chrome.runtime.getURL("styles.css");
-document.querySelector("head").innerHTML += '<link rel="stylesheet" type="text/css" href="' + a + '" >';
