ee3ce0fea5d7ad24b120f0afe97ea76455a05998
JacobSwihart <70299421+JacobSwihart@users.noreply.github.com>
2023-10-17 22:33:36 -0400

    Added Right Click Support
    Made it so the right click works with the changed links too

--- Assignment 5 Extension/content.js
+++ Assignment 5 Extension/content.js
@@ -8,8 +8,11 @@ for(let i = 0; i < el.length; i++){
     //stores the url before changing it to a function that does nothing
     el[i].dataset.url = el[i].href;
     el[i].href = "javascript:void(0);"
-    //when the link is clicked, the original url is restored, allowing the links to remain functional
-    el[i].onclick = (function(){
-        this.href = this.dataset.url;
-    });
+    //links the url reset function to the left and right click
+    el[i].onclick = resetURL;
+    el[i].oncontextmenu = resetURL;
+}
+//when the link is clicked, the original url is restored, allowing the links to remain functional
+function resetURL(e){
+    this.href = this.dataset.url;
 }
